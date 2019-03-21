c5fcfb7ee4661539cff72dd2e74a3c7259f1b23a
Irving Sulub <ikiszulu@gmail.com>
2019-03-20 19:22:59 -0600

    cdwrfs

--- Proyecto final/GUI/Cliente Esporadico.Designer.cs
+++ Proyecto final/GUI/Cliente Esporadico.Designer.cs
@@ -31,6 +31,15 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.pnl_Fondo = new System.Windows.Forms.Panel();
+            this.txt_Placas = new System.Windows.Forms.TextBox();
+            this.txt_Numero_Cajon = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Buscar2 = new System.Windows.Forms.Button();
+            this.btn_Agregar2 = new System.Windows.Forms.Button();
+            this.btn_Eliminar2 = new System.Windows.Forms.Button();
+            this.btn_Modificar2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.pnl_Fondo.SuspendLayout();
             this.SuspendLayout();
@@ -38,6 +47,14 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.btn_Modificar2);
+            this.panel1.Controls.Add(this.btn_Eliminar2);
+            this.panel1.Controls.Add(this.btn_Agregar2);
+            this.panel1.Controls.Add(this.btn_Buscar2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txt_Numero_Cajon);
+            this.panel1.Controls.Add(this.txt_Placas);
             this.panel1.Location = new System.Drawing.Point(23, 24);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(532, 128);
@@ -61,6 +78,74 @@
             this.pnl_Fondo.Size = new System.Drawing.Size(578, 404);
             this.pnl_Fondo.TabIndex = 2;
             // 
+            // txt_Placas
+            // 
+            this.txt_Placas.Location = new System.Drawing.Point(32, 38);
+            this.txt_Placas.Name = "txt_Placas";
+            this.txt_Placas.Size = new System.Drawing.Size(100, 20);
+            this.txt_Placas.TabIndex = 0;
+            // 
+            // txt_Numero_Cajon
+            // 
+            this.txt_Numero_Cajon.Location = new System.Drawing.Point(138, 38);
+            this.txt_Numero_Cajon.Name = "txt_Numero_Cajon";
+            this.txt_Numero_Cajon.Size = new System.Drawing.Size(100, 20);
+            this.txt_Numero_Cajon.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numero de placas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero de cajon";
+            // 
+            // btn_Buscar2
+            // 
+            this.btn_Buscar2.Location = new System.Drawing.Point(32, 77);
+            this.btn_Buscar2.Name = "btn_Buscar2";
+            this.btn_Buscar2.Size = new System.Drawing.Size(75, 23);
+            this.btn_Buscar2.TabIndex = 4;
+            this.btn_Buscar2.Text = "Buscar";
+            this.btn_Buscar2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Agregar2
+            // 
+            this.btn_Agregar2.Location = new System.Drawing.Point(127, 77);
+            this.btn_Agregar2.Name = "btn_Agregar2";
+            this.btn_Agregar2.Size = new System.Drawing.Size(75, 23);
+            this.btn_Agregar2.TabIndex = 5;
+            this.btn_Agregar2.Text = "Agregar";
+            this.btn_Agregar2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Eliminar2
+            // 
+            this.btn_Eliminar2.Location = new System.Drawing.Point(225, 77);
+            this.btn_Eliminar2.Name = "btn_Eliminar2";
+            this.btn_Eliminar2.Size = new System.Drawing.Size(75, 23);
+            this.btn_Eliminar2.TabIndex = 6;
+            this.btn_Eliminar2.Text = "Eliminar";
+            this.btn_Eliminar2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Modificar2
+            // 
+            this.btn_Modificar2.Location = new System.Drawing.Point(320, 77);
+            this.btn_Modificar2.Name = "btn_Modificar2";
+            this.btn_Modificar2.Size = new System.Drawing.Size(75, 23);
+            this.btn_Modificar2.TabIndex = 7;
+            this.btn_Modificar2.Text = "modificar";
+            this.btn_Modificar2.UseVisualStyleBackColor = true;
+            // 
             // Cliente_Esporadico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -71,6 +156,8 @@
             this.Style = MetroFramework.MetroColorStyle.Teal;
             this.Text = "Cliente Esporadico";
             this.TransparencyKey = System.Drawing.Color.Azure;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.pnl_Fondo.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -82,5 +169,13 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Panel pnl_Fondo;
+        private System.Windows.Forms.Button btn_Modificar2;
+        private System.Windows.Forms.Button btn_Eliminar2;
+        private System.Windows.Forms.Button btn_Agregar2;
+        private System.Windows.Forms.Button btn_Buscar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Numero_Cajon;
+        private System.Windows.Forms.TextBox txt_Placas;
     }
 }
